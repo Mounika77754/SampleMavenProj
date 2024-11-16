b2898cdaf1af2a3dd0181954f8ce2ec8da6429c2
Mounika77754 <mouni77754@gmail.com>
2024-11-16 15:33:04 +0530

    sample maven java

---
+++ src/main/java/cpm/app/se/SampleMavenProj/App.java
@@ -0,0 +1,13 @@
+package cpm.app.se.SampleMavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
